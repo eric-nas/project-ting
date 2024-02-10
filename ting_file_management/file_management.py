a51bc3c220a1878bb813c9747dfffdb7be8fe9f7
eric-nas <ericnascimentocruzc@gmail.com>
2024-02-10 16:37:54 -0300

    Req2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.find('.txt') < 0:
+        return print('Formato inválido', file=sys.stderr)
+    if os.path.exists(path_file):
+        with open(path_file, "r") as file:
+            content = file.readlines()
+            content_sem_quebras = [linha.strip() for linha in content]
+            return content_sem_quebras
+    else:
+        return print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
