de93ed28d96f3fc8b6eedfb65fb34cc78b44bd6b
eric-nas <ericnascimentocruzc@gmail.com>
2024-02-11 15:01:07 -0300

    Req7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word, instance: Queue):
+    result = []
+    for archive in instance.data:
+        linhas = archive['linhas_do_arquivo']
+        for indice, linha in enumerate(linhas, start=1):
+            if word in linha.lower():
+                result.append({"linha": indice})
+        if result == []:
+            return result
+        return [{
+            "palavra": word,
+            "arquivo": archive['nome_do_arquivo'],
+            "ocorrencias": result
+        }]
 
 
 def search_by_word(word, instance):
